d5a90066b066eeec7ba7d5effdcdb81d25b4c0fc
Kauê Schaefer de Andrade <128431286+kauesandrade@users.noreply.github.com>
2025-05-27 22:53:16 -0300

    Add api

---
+++ apiNode/server.js
@@ -0,0 +1,99 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(express.json());
+
+app.use(cors({
+  origin: '*' // <- para testes e desenvolvimento
+}));
+
+let tarefa = [{id: 1, title: 'Tarefa 1', status: 'pendente'}];
+let idSequence = 1;
+
+app.get('/tarefa', (req, res) => {
+    res.json(tarefa);
+});
+
+app.post('/tarefa', (req, res) => {
+    const { title } = req.body;
+
+    if (!title) {
+        res.status(400).json({error: "Nome é obrigatório"});
+        return;
+    }
+
+    idSequence++;
+    const newTarefa = {
+        id: idSequence,
+        title,
+        status: 'pendente'
+    };
+
+    tarefa.push(newTarefa);
+    res.status(201).json(newTarefa);
+})
+
+app.put('/tarefa/:id/title', (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+
+    const tarefaIndex = tarefa.findIndex(t => t.id == id);
+
+    if (tarefaIndex === -1) {
+        res.status(404).json({error: "Tarefa não encontrada"});
+        return;
+    }
+
+    if (!title) {
+        res.status(400).json({error: "Nome e status são obrigatórios"});
+        return;
+    }
+
+    tarefa[tarefaIndex] = { id, title, status: tarefa[tarefaIndex].status }; 
+    res.json(tarefa[tarefaIndex]);
+});
+
+app.put('/tarefa/:id/status', (req, res) => {
+    const { id } = req.params;
+    const { status } = req.body;
+
+    const tarefaIndex = tarefa.findIndex(t => t.id == id);
+
+    if (tarefaIndex === -1) {
+        res.status(404).json({error: "Tarefa não encontrada"});
+        return;
+    }
+
+    if (!status) {
+        res.status(400).json({error: "Nome e status são obrigatórios"});
+        return;
+    }
+
+    tarefa[tarefaIndex] = { id, title: tarefa[tarefaIndex].title, status };
+    res.json(tarefa[tarefaIndex]);
+});
+
+app.delete('/tarefa/:id', (req, res) => {
+    const { id } = req.params;
+    const tarefaIndex = tarefa.findIndex(t => t.id == id);
+
+    if (tarefaIndex === -1) {
+        res.status(404).json({error: "Tarefa não encontrada"});
+        return;
+    }
+
+    tarefa.splice(tarefaIndex, 1);
+    res.status(204).send();
+});
+
+app.listen(8000, () => {
+    console.log('Servidor rodando na porta 8000');
+});
+
+// fetch('http://localhost:3000/tarefa', {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({ nome: 'Tarefa 2', status: 'pendente' })
+// })
--- javascript_2/script.js
+++ javascript_2/script.js
@@ -1,33 +1,83 @@
-const URL = "https://jsonplaceholder.typicode.com/posts"
+const URL = "http://localhost:8000/tarefa";
+
+async function removerCard(id) {
+    const response = await fetch(`${URL}/${id}`, {
+        method: 'DELETE'
+    });
+    if (response.ok) {
+        const card = document.getElementById(id);
+        card.remove();
+        console.log('Card removido com sucesso');
+    } else {
+        console.error('Erro ao remover o card');
+    }
+}
+
+async function adicionarCard(title) {
+    const response = await fetch(URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ title, status: 'pendente' })
+    });
+
+    if (response.ok) {
+        const converteAPromessa = await response.json()
+        criarCard(converteAPromessa.id, converteAPromessa.title, converteAPromessa.status);
+        console.log('Card adicionado com sucesso');
+    } else {
+        console.error('Erro ao adicionar o card');
+    }
+}
+
+async function editarTitleCard(id, novoValor) {
+    const response = await fetch(`${URL}/${id}/title`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ title: novoValor })
+    });
+
+    if (response.ok) {
+        const converteAPromessa = await response.json();
+        const card = document.getElementById(id);
+        card.querySelector('p').innerHTML = converteAPromessa.title;
+        console.log('Card editado com sucesso');
+    } else {
+        console.error('Erro ao editar o card');
+    }
+}
+
+async function editarStatusCard(id, statusVelho) {
+
+    const novoStatus = statusVelho === 'pendente' ? 'feito' : 'pendente';
+
+    const response = await fetch(`${URL}/${id}/status`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ status: novoStatus })
+    });
+
+    if (response.ok) {
+        const card = document.getElementById(id);
+        card.querySelector('.status-' + statusVelho).className = 'status-' + novoStatus;
+        console.log('Status do card editado com sucesso');
+    } else {
+        console.error('Erro ao editar o status do card');
+    }
+}
+
 
 async function carregarDados() {
     const retornoDaPromessa = await fetch(URL);
     const converteAPromessa = await retornoDaPromessa.json()
-    const listaDePostagem = document.getElementById("listaDePostagem");
+    console.log(converteAPromessa);
     for (let i = 0; i < converteAPromessa.length; i++) {
-        const tagLi = document.createElement("li");
-        tagLi.innerHTML = converteAPromessa[i].title
-        tagLi.className = converteAPromessa[i].title
-        const botaoRemover =  document.createElement("button");
-        const divBotao = document.createElement("div");
-        botaoRemover.innerHTML = "REMOVER";
-        botaoRemover.onclick = () => {
-            tagLi.remove();
-        };
-        const botaoEditar =  document.createElement("button");
-        botaoEditar.innerHTML = "EDITAR";
-        botaoEditar.onclick = () => {
-            const novoValor = prompt(`Digite o novo valor para o item: `);
-            tagLi.innerText = novoValor;
-            tagLi.className = novoValor;
-            divBotao.appendChild(botaoRemover);
-            divBotao.appendChild(botaoEditar);
-            tagLi.appendChild(divBotao);
-        };
-        divBotao.appendChild(botaoRemover);
-        divBotao.appendChild(botaoEditar);
-        tagLi.appendChild(divBotao);
-        listaDePostagem.appendChild(tagLi);
+        criarCard(converteAPromessa[i].id, converteAPromessa[i].title, converteAPromessa[i].status);
     }
 }
 
@@ -39,32 +89,36 @@ botaoAdicionar.onclick = () => {
     if (!inputAdicionar.value) {
         alert("Digite um valor no campo de adição de novos itens!");
         return;
+    } else {
+        adicionarCard(inputAdicionar.value);
     }
 
-    const listaDePostagem = document.getElementById("listaDePostagem");
-    const divBotao = document.createElement('div');
-    const tagLi = document.createElement("li");
-    tagLi.innerHTML = inputAdicionar.value
-    tagLi.className = inputAdicionar.value
-    const botaoRemover =  document.createElement("button");
-    botaoRemover.innerHTML = "REMOVER";
-    botaoRemover.onclick = () => {
-        tagLi.remove();
-    };
-    const botaoEditar =  document.createElement("button");
-    botaoEditar.innerHTML = "EDITAR";
-    botaoEditar.onclick = () => {
-        const novoValor = prompt(`Digite o novo valor para o item: `);
-        tagLi.innerText = novoValor;
-        tagLi.className = novoValor;
-        divBotao.appendChild(botaoRemover);
-        divBotao.appendChild(botaoEditar);
-        tagLi.appendChild(divBotao);
-    };
-    divBotao.appendChild(botaoRemover);
-    divBotao.appendChild(botaoEditar);
-    tagLi.appendChild(divBotao);
-    listaDePostagem.appendChild(tagLi)
+    inputAdicionar.value = "";
+
+    // const listaDePostagem = document.getElementById("listaDePostagem");
+    // const divBotao = document.createElement('div');
+    // const articleCard = document.createElement("li");
+    // articleCard.innerHTML = inputAdicionar.value
+    // articleCard.className = inputAdicionar.value
+    // const botaoRemover = document.createElement("button");
+    // botaoRemover.innerHTML = "REMOVER";
+    // botaoRemover.onclick = () => {
+    //     articleCard.remove();
+    // };
+    // const botaoEditar = document.createElement("button");
+    // botaoEditar.innerHTML = "EDITAR";
+    // botaoEditar.onclick = () => {
+    //     const novoValor = prompt(`Digite o novo valor para o item: `);
+    //     articleCard.innerText = novoValor;
+    //     articleCard.className = novoValor;
+    //     divBotao.appendChild(botaoRemover);
+    //     divBotao.appendChild(botaoEditar);
+    //     articleCard.appendChild(divBotao);
+    // };
+    // divBotao.appendChild(botaoRemover);
+    // divBotao.appendChild(botaoEditar);
+    // articleCard.appendChild(divBotao);
+    // listaDePostagem.appendChild(articleCard)
 
 };
 
@@ -73,13 +127,56 @@ const botaoFiltro = document.getElementById("botaoFiltro");
 botaoFiltro.onclick = () => {
     const listaDePostagem = document.getElementById("listaDePostagem");
     for (let i = 0; i < listaDePostagem.childElementCount; i++) {
-        const elemento = listaDePostagem.children[i];
-        if (!elemento.className.includes(filtro.value)) {
-            elemento.style.display = "none";
+        const card = listaDePostagem.children[i];
+        const pTexto = card.querySelector('p');
+        if (!pTexto.className.includes(filtro.value)) {
+            card.style.display = "none";
         } else {
-            elemento.style.display = "flex";
+            card.style.display = "";
         }
     }
+}
+
+const criarCard = (id, title, status) => {
+    const listaDePostagem = document.getElementById("listaDePostagem");
+
+        const articleCard = document.createElement("Article");
+        articleCard.id = id;
+
+        const statusDiv = document.createElement("div");
+        statusDiv.className = 'status-' + status;
+        statusDiv.onclick = () => {
+            const statusAtual = statusDiv.className.split('-')[1];
+            editarStatusCard(articleCard.id, statusAtual);
+        }
+        articleCard.appendChild(statusDiv);
+
+        const pTexto = document.createElement("p");
+        pTexto.innerHTML = title;
+        pTexto.className = title;
+        articleCard.appendChild(pTexto);
+
+        const divBotao = document.createElement("div");
+        divBotao.className = 'buttons';
+
+        const botaoRemover = document.createElement("button");
+        botaoRemover.className = 'remover';
+        botaoRemover.innerHTML = "REMOVER";
+        botaoRemover.onclick = () => {
+            removerCard(articleCard.id)
+        };
+
+        const botaoEditar = document.createElement("button");
+        botaoEditar.className = 'editar';
+        botaoEditar.innerHTML = "EDITAR";
+        botaoEditar.onclick = () => {
+            const novoValor = prompt(`Digite o novo valor para o item: `);
+            editarTitleCard(articleCard.id, novoValor);
+        };
+        divBotao.appendChild(botaoRemover);
+        divBotao.appendChild(botaoEditar);
+        articleCard.appendChild(divBotao);
+        listaDePostagem.appendChild(articleCard);
 }
 
 carregarDados();
